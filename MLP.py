90783245413c1a371fe9a5a256b0c22ca0dc35a5
antk99 <triiggr@gmail.com>
2022-04-04 08:42:19 -0400

    Added sklearn code

--- MLP.py
+++ MLP.py
@@ -1,6 +1,8 @@
 import numpy as np
 import warnings
 
+import sklearn.neural_network
+
 warnings.filterwarnings('ignore')
 
 
@@ -23,6 +25,10 @@ def evaluate_acc(y_test, y_pred):
     return np.sum(y_pred == y_test) / y_pred.shape[0]
 
 
+def one_hot(a, num_classes):
+    return np.squeeze(np.eye(num_classes)[a.reshape(-1)])
+
+
 class MLP:
 
     def __init__(self, num_inputs, num_outputs, num_hidden_units=[64, 64], activation_fn=ReLu):
@@ -37,10 +43,11 @@ class MLP:
 
         # initializing weights and biases (bias ignored for now)
         for i in range(len(layers) - 1):
-            self.weights.append(np.random.randn(layers[i], layers[i+1]) * .01)  # idk why * .01 it was in the slides
+            self.weights.append(np.random.randn(layers[i], layers[i+1]) * 0.75)  # idk why * .01 it was in the slides
 
     def fit(self, x, y):
         N, D = x.shape
+        y = one_hot(y, self.num_outputs)
 
         def gradient1(x, y, params):
             v, w = params
@@ -52,9 +59,27 @@ class MLP:
             dw = np.dot(z.T, dy) / N  # M x C
             dz = np.dot(dy, w.T)    # N x M
             dv = np.dot(x.T, dz * q_01)/N   # D x M
-            return [dw, dv]
+            return [dv, dw]
 
-        self.params = GradientDescent().run(gradient1, x, y, [self.weights[0], self.weights[1]])
+        learning_rate = 0.001
+        max_iters = 1e4
+        epsilon = 1e-8
+
+        norms = np.array([np.inf])
+        t = 1
+        while np.any(norms > epsilon) and t < max_iters:
+            grad = gradient1(x, y, self.weights)
+            for p in range(len(self.weights)):
+                if np.isnan(np.min(grad[p])):
+                    t = max_iters
+                    break
+
+                self.weights[p] = self.weights[p] - (learning_rate * grad[p])
+            t += 1
+            norms = np.array([np.linalg.norm(g) for g in grad])
+            if (t % 10) == 0:
+                print(f"{t/max_iters*100}% complete")
+
         return self
 
     def predict(self, x):
@@ -64,24 +89,26 @@ class MLP:
         return output
 
 
-class GradientDescent:
-
-    def __init__(self, learning_rate=.001, max_iters=1e4, epsilon=1e-8):
-        self.learning_rate = learning_rate
-        self.max_iters = max_iters
-        self.epsilon = epsilon
-
-    def run(self, gradient_fn, x, y, params):
-        norms = np.array([np.inf])
-        t = 1
-        while np.any(norms > self.epsilon) and t < self.max_iters:
-            grad = gradient_fn(x, y, params)
-            for p in range(len(params)):
-                params[p] -= self.learning_rate * grad[p]
-            t += 1
-            norms = np.array([np.linalg.norm(g) for g in grad])
-        return params
-
-
 if __name__ == '__main__':
-    pass
+    import numpy as np
+    import mnist_reader
+
+    X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
+    X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+
+    # Data normalization
+    X_train = X_train - np.mean(X_train)
+    X_train = X_train / np.std(X_train)
+    X_test = X_test - np.mean(X_test)
+    X_test = X_test / np.std(X_test)
+
+    # model = MLP(X_train.shape[1], 10, [128])
+    # model.fit(X_train, y_train)
+    # y_pred = np.argmax(model.predict(X_test), axis=1)
+    # print(y_pred)
+    # print(evaluate_acc(y_test, y_pred))
+
+    model = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(128, 128), activation='tanh', solver='sgd')
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print(evaluate_acc(y_test, y_pred))
