c0d84f945a276e786aa0488b2ebcdacb37944deb
antk99 <triiggr@gmail.com>
2022-04-04 00:03:51 -0400

    Modified gradient function

--- MLP.py
+++ MLP.py
@@ -3,39 +3,65 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-logistic = lambda z: 1. / (1 + np.exp(-z))
+
+def ReLu(arr):
+    return np.maximum(arr, 0)
+
+
+def softmax(arr):
+    e = np.exp(arr)
+    return e/e.sum()
+
+
+def evaluate_acc(y_test, y_pred):
+    """
+    Evaluates the accuracy of a model's prediction
+    :param y_test: np.ndarray - the true labels
+    :param y_pred: np.ndarray - the predicted labels
+    :return: float - prediction accuracy
+    """
+    return np.sum(y_pred == y_test) / y_pred.shape[0]
 
 
 class MLP:
 
-    def __init__(self, M=64):
-        self.M = M
+    def __init__(self, num_inputs, num_outputs, num_hidden_units=[64, 64], activation_fn=ReLu):
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.num_layers = len(num_hidden_units)
+        self.size_layers = num_hidden_units
+        self.activation_fn = activation_fn
 
-    def fit(self, x, y, optimizer):
+        self.weights = []
+        layers = [num_inputs] + num_hidden_units + [num_outputs]
+
+        # initializing weights and biases (bias ignored for now)
+        for i in range(len(layers) - 1):
+            self.weights.append(np.random.randn(layers[i], layers[i+1]) * .01)  # idk why * .01 it was in the slides
+
+    def fit(self, x, y):
         N, D = x.shape
 
-        def gradient(x, y, params):
+        def gradient1(x, y, params):
             v, w = params
-            z = logistic(np.dot(x, v))  # N x M
-            yh = logistic(np.dot(z, w))  # N
+            q = np.dot(x, v)
+            q_01 = np.where(q > 0, 1, 0)
+            z = ReLu(q)  # N x M
+            yh = softmax(np.dot(z, w))  # N
             dy = yh - y  # N
-            dw = np.dot(z.T, dy) / N  # M
-            dz = np.outer(dy, w)  # N x M
-            dv = np.dot(x.T, dz * z * (1 - z)) / N  # D x M
-            dparams = [dv, dw]
-            return dparams
+            dw = np.dot(z.T, dy) / N  # M x C
+            dz = np.dot(dy, w.T)    # N x M
+            dv = np.dot(x.T, dz * q_01)/N   # D x M
+            return [dw, dv]
 
-        w = np.random.randn(self.M) * .01
-        v = np.random.randn(D, self.M) * .01
-        params0 = [v, w]
-        self.params = optimizer.run(gradient, x, y, params0)
+        self.params = GradientDescent().run(gradient1, x, y, [self.weights[0], self.weights[1]])
         return self
 
     def predict(self, x):
-        v, w = self.params
-        z = logistic(np.dot(x, v))  # N x M
-        yh = logistic(np.dot(z, w))  # N
-        return yh
+        output = x
+        for weight_matrix in self.weights:
+            output = self.activation_fn(np.dot(output, weight_matrix))
+        return output
 
 
 class GradientDescent:
@@ -55,3 +81,7 @@ class GradientDescent:
             t += 1
             norms = np.array([np.linalg.norm(g) for g in grad])
         return params
+
+
+if __name__ == '__main__':
+    pass
